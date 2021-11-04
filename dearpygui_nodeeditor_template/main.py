7718c14186d79b0622e7c08073d14b207b034fb3
Patrick K <luminouslizard@noreply.codeberg.org>
2021-11-04 22:25:24 +0100

    Update 'dearpygui_nodeeditor_template/main.py'
    update o Dearpygui 1.0.2

--- dearpygui_nodeeditor_template/main.py
+++ dearpygui_nodeeditor_template/main.py
@@ -1,9 +1,13 @@
 # Copyright 2021 LuminousLizard
 # Licensed under the MIT-License
 
-from dearpygui.core import *
-from dearpygui.simple import *
-from src.node_editor import callback_show_node_editor
+import dearpygui.dearpygui as dpg
+from src.node_editor import NodeEditor
+
+dpg.create_context()
+dpg.create_viewport(title="Node Editor Template",
+                    width=1500,
+                    height=768)
 
 
 def callback_close_program(sender, data):
@@ -11,34 +15,29 @@ def callback_close_program(sender, data):
 
 
 def callback_show_debugger(sender, data):
-    show_debug()
+    dpg.show_debug()
 
 
-def callback_show_logger(sender, data):
-    show_logger()
+with dpg.font_registry():
+    default_font = dpg.add_font("fonts/OpenSans-Regular.ttf", 15)
 
 
-with window("MainWindow",
-            x_pos=0,
-            y_pos=0,
-            width=700,
-            height=200):
+with dpg.viewport_menu_bar():
+    dpg.add_menu_item(label="Debugger", callback=callback_show_debugger)
+    dpg.add_menu_item(label="Close", callback=callback_close_program)
 
-    set_theme("Cherry")
-    set_main_window_size(1500, 900)
-    add_additional_font("dearpygui_nodeeditor_template/fonts/OpenSans-Regular.ttf", 20)
 
-    with menu_bar("MainMenuBar"):
-        with menu("Node editor"):
-            add_menu_item("Open a new node editor", callback=callback_show_node_editor)
-        add_menu_item("Debugger", callback=callback_show_debugger)
-        add_menu_item("Logger", callback=callback_show_logger)
+with dpg.window(label="MainWindow",
+                pos=[50, 50],
+                width=700,
+                height=200):
+    dpg.bind_font(default_font)
 
-        add_menu_item("Close", callback=callback_close_program)
+    nodeEditor = NodeEditor()
 
-    add_text("Start a node editor from the menu bar.\n"
-             "Nodes (here only one) can be added via the menu bar or context menu (right mouse click).\n"
-             "Scrolling is possible via middle mouse button.\n"
-             "See \"node_editor.py\" how it works, I have written in some comments.")
 
-start_dearpygui()
+# Main Loop
+dpg.show_viewport()
+dpg.setup_dearpygui()
+dpg.start_dearpygui()
+dpg.destroy_context()
