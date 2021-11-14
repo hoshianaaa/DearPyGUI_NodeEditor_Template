a4766d87be7436510f847784930bb81703c3c5d3
Patrick K <luminouslizard@noreply.codeberg.org>
2021-11-14 16:28:35 +0100

    Upload files to 'dearpygui_nodeeditor_template/src'

---
+++ dearpygui_nodeeditor_template/src/node_function_links.py
@@ -0,0 +1,16 @@
+# Copyright 2021 LuminousLizard
+# Licensed under the EUPL-1.2
+
+from src.node_functions import *
+
+
+def linking_node_function(node_tag):
+    link_dict = {
+        "Node_Addition": node_addition_func.calc_addition,
+        "Node_Subtraction": node_subtraction_func.calc_subtraction,
+        "Multiplication": node_multiplication_func.calc_multiplication,
+        "Division": node_division_func.calc_division
+    }
+    for key in link_dict:
+        if key in node_tag[1]:
+            link_dict[key](node_tag[0])
