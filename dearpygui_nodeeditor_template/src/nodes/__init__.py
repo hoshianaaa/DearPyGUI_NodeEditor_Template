d85c17a72db1bedf11247a97b168d6d810e3811c
Patrick K <luminouslizard@noreply.codeberg.org>
2021-11-14 16:31:08 +0100

    Update 'dearpygui_nodeeditor_template/src/nodes/__init__.py'

--- dearpygui_nodeeditor_template/src/nodes/__init__.py
+++ dearpygui_nodeeditor_template/src/nodes/__init__.py
@@ -1,4 +1,5 @@
 # Copyright 2021 LuminousLizard
 # Licensed under the MIT-License
 
-__all__ = ["node_addition", "node_input_float", "node_output_float"]
+__all__ = ["node_input_float", "node_output_float", "node_addition", "node_subtraction", "node_multiplication",
+           "node_division"]
